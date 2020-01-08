be2aba613c73d37dda2a61151995fd5e0db045d0
wissal <wissal.ouarsafi@gmail.com>
2020-01-08 13:08:29 -0800

    Draw the vertical lines of our maze inside our canvas using js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ const stepThroughCell = (row, column) => {
 
 stepThroughCell(startRow, startColumn)
 
-//Drawing our cell in our canvas
+//Drawing our cells in our canvas
 
 horizontals.forEach((row, rowIndex) => {
    row.forEach((open, columnIndex) => {
@@ -123,6 +123,25 @@ horizontals.forEach((row, rowIndex) => {
            rowIndex * unitLength + unitLength,
            unitLength,
            10,
+           {
+               isStatic: true
+           }
+       );
+       World.add(world, wall)
+   });
+});
+
+verticals.forEach((row, rowIndex) => {
+   row.forEach((open, columnIndex) => {
+       if(open){
+           return;
+       }
+
+       const wall = Bodies.rectangle(
+           columnIndex * unitLength + unitLength,
+           rowIndex * unitLength + unitLength / 2,
+           10,
+           unitLength,
            {
                isStatic: true
            }

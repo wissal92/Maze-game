6519107c47b37c895eceebb4868cb5dd6089a581
wissal <wissal.ouarsafi@gmail.com>
2020-01-08 12:14:17 -0800

    Finish the functionality responsible for generating the maze with its corresponding walls

--- index.js
+++ index.js
@@ -71,13 +71,39 @@ const stepThroughCell = (row, column) => {
 
    //Assemble randomly the list of neighbors:
    const neighbors = shuffle([
-       [row - 1, column],
-       [row , column + 1],
-       [row + 1, column],
-       [row , column - 1]
+       [row - 1, column, 'up'],
+       [row , column + 1, 'right'],
+       [row + 1, column, 'down'],
+       [row , column - 1, 'left']
    ]);
+   
+   //for each neighbor:
+   for(let neighbor of neighbors){
+       const [nextRow, nextColumn, direction] = neighbor;
 
-   console.log(neighbors)
+       //see if the neighbor is out of bounds
+       if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells){
+           continue;
+       }
+
+       //if we have visited that neighbor, continue to the next neighbor
+       if(grid[nextRow][nextColumn]){
+           continue;
+       }
+
+       //Remove a wall from either horizontals or verticals:
+       if(direction === 'left'){
+           verticals[row][column - 1] = true;
+       } else if(direction === 'right'){
+           verticals[row][column] = true;
+       } else if(direction === 'up'){
+           horizontals[row - 1][column] = true;
+       } else if (direction === 'down'){
+           horizontals[row][column] = true;
+       }
+
+       stepThroughCell(nextRow, nextColumn)
+   }
 }
 
 stepThroughCell(startRow, startColumn)

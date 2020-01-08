f1bee04e07e6956c1ed260d3c8abb8e6db1659a4
wissal <wissal.ouarsafi@gmail.com>
2020-01-08 13:24:41 -0800

    Draw the goal in our maze

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {Engine, Render, Runner, World, Bodies} = Matter;
 
-const cells = 3;
+const cells = 13;
 const width = 600;
 const height = 600;
 
@@ -23,10 +23,10 @@ Runner.run(Runner.create(), engine);
 
 //Walls
 const walls = [
-    Bodies.rectangle(width /2 , 0, width, 40, {isStatic: true}),
-    Bodies.rectangle(width /2, height, width, 40, {isStatic: true}),
-    Bodies.rectangle(0, height/2, 40, height, {isStatic: true}),
-    Bodies.rectangle(width, height/2, 40, height, {isStatic: true})
+    Bodies.rectangle(width /2 , 0, width, 2, {isStatic: true}),
+    Bodies.rectangle(width /2, height, width, 2, {isStatic: true}),
+    Bodies.rectangle(0, height/2, 2, height, {isStatic: true}),
+    Bodies.rectangle(width, height/2, 2, height, {isStatic: true})
 ];
 World.add(world, walls);
 
@@ -122,7 +122,7 @@ horizontals.forEach((row, rowIndex) => {
            columnIndex * unitLength + unitLength / 2,
            rowIndex * unitLength + unitLength,
            unitLength,
-           10,
+           5,
            {
                isStatic: true
            }
@@ -140,7 +140,7 @@ verticals.forEach((row, rowIndex) => {
        const wall = Bodies.rectangle(
            columnIndex * unitLength + unitLength,
            rowIndex * unitLength + unitLength / 2,
-           10,
+           5,
            unitLength,
            {
                isStatic: true
@@ -149,3 +149,15 @@ verticals.forEach((row, rowIndex) => {
        World.add(world, wall)
    });
 });
+
+//Create our goal:
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * 0.7,
+    unitLength * 0.7,
+    {
+        isStatic: true
+    }
+);
+World.add(world, goal)
